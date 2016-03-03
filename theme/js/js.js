d82d2cfcca4bf025963ed4c40e29b6ff24bd7aa8
Musaev Ruslan <musya.wow@gmail.com>
2016-03-03 10:41:25 +0300

    first

---
+++ theme/js/js.js
@@ -0,0 +1,13 @@
+(function ($) {
+
+  $(window).load(function() {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      controlNav: false,
+      prevText: "",
+      nextText: ""
+    });
+  });
+
+})(jQuery);
+
